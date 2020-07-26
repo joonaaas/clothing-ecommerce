007d36a1094447e8b7ba47e6914d5e34cdc46927
Jonas Alolino <jonasqalolino@gmail.com>
2020-07-26 23:12:19 +0800

    updated menu-items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './homepage.styles.scss'
 
 import Directory from '../../components/directory/directory.component'
+
 const HomePage = () => (
 	<div className='homepage'>
-		<Directory/>
+		<Directory />
 	</div>
 )
 
