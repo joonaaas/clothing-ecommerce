d542346b326cfeade08b08b1f1d9e179e9040cfe
Jonas Alolino <jonasqalolino@gmail.com>
2020-10-12 23:00:19 +0800

    Update our App component, Sign-in comp, Sign-up component and shop component to use hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-import FromInput from '../form-input/form-input.component'
+import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
 import { SignInContainer, ButtonComponent } from './sign-in.styles'
@@ -12,65 +12,60 @@ import {
 
 import { connect } from 'react-redux'
 
-class SignIn extends Component {
-	state = {
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+	const [userCredentials, setUserCredentials] = useState({
 		email: '',
 		password: '',
-	}
+	})
 
-	handleSubmit = async (e) => {
+	const { email, password } = userCredentials
+
+	const handleSubmit = async (e) => {
 		e.preventDefault()
-
-		const { emailSignInStart } = this.props
-		const { email, password } = this.state
 
 		emailSignInStart(email, password)
 	}
 
-	handleChange = (e) => {
+	const handleChange = (e) => {
 		const { value, name } = e.target
 
-		this.setState({ [name]: value })
+		setUserCredentials({ ...userCredentials, [name]: value })
 	}
 
-	render() {
-		const { googleSignInStart } = this.props
+	return (
+		<SignInContainer>
+			<h2>I already have an account</h2>
+			<span>Sign in with your email and password</span>
 
-		return (
-			<SignInContainer>
-				<h2>I already have an account</h2>
-				<span>Sign in with your email and password</span>
-
-				<form onSubmit={this.handleSubmit}>
-					<FromInput
-						type='email'
-						name='email'
-						label='Email'
-						value={this.state.email}
-						handleChange={this.handleChange}
-						required
-					/>
-					<FromInput
-						type='password'
-						name='password'
-						label='Password'
-						value={this.state.password}
-						handleChange={this.handleChange}
-						required
-					/>
-					<ButtonComponent>
-						<CustomButton type='submit'>Sign In</CustomButton>
-						<CustomButton
-							type='button'
-							onClick={googleSignInStart}
-							isGoogleSignIn>
-							Sign In with Google
-						</CustomButton>
-					</ButtonComponent>
-				</form>
-			</SignInContainer>
-		)
-	}
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					type='email'
+					name='email'
+					label='Email'
+					value={email}
+					handleChange={handleChange}
+					required
+				/>
+				<FormInput
+					type='password'
+					name='password'
+					label='Password'
+					value={password}
+					handleChange={handleChange}
+					required
+				/>
+				<ButtonComponent>
+					<CustomButton type='submit'>Sign In</CustomButton>
+					<CustomButton
+						type='button'
+						onClick={googleSignInStart}
+						isGoogleSignIn>
+						Sign In with Google
+					</CustomButton>
+				</ButtonComponent>
+			</form>
+		</SignInContainer>
+	)
 }
 
 const mapDispatchToProps = (dispatch) => ({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 
@@ -7,47 +7,44 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionContainer from '../collection/collection.container'
 
-class ShopPage extends React.Component {
-	componentDidMount() {
-		const { fetchCollectionsStart } = this.props
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+	useEffect(() => {
 		fetchCollectionsStart()
-	}
+	}, [fetchCollectionsStart])
+	// ? this will execute if [fetchCollectionsStart] change
 
-	render() {
-		const { match } = this.props
-		return (
-			<div className='shop-page'>
-				<Route
-					exact
-					path={`${match.path}`}
-					component={CollectionsOverviewContainer}
-					// ? 👆 before 👇
-					// render={(props) => (
-					// 	<CollectionsOverviewWithSpinner
-					// 		isLoading={isCollectionFetching} // if true then will show spinner
-					// 		{...props}
-					// 	/>
-					// )}
-				/>
-				<Route
-					path={`${match.path}/:collectionId`}
-					component={CollectionContainer}
-					// ? 👆 before 👇
-					// render={(props) => (
-					// 	<CollectionsPageWithSpinner
-					// 		isLoading={!isCollectionsLoaded}
-					// 		  ? 👆 we reverse the boolean since we want to stop loading when true;					 							there's an object so we want to stop spinning
-					// 		{...props}
-					// 	/>
-					// )}
-				/>
-			</div>
-		)
-		// *** BEFORE *** 👇
-		// <Route exact path={`${match.path}`} component={CollectionsOverview} />
-		// <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-		// ? we put render={} in the <Route/> props instead of component={}; edited in folder (17.HOC, Video.2)
-	}
+	return (
+		<div className='shop-page'>
+			<Route
+				exact
+				path={`${match.path}`}
+				component={CollectionsOverviewContainer}
+				// ? 👆 before 👇
+				// render={(props) => (
+				// 	<CollectionsOverviewWithSpinner
+				// 		isLoading={isCollectionFetching} // if true then will show spinner
+				// 		{...props}
+				// 	/>
+				// )}
+			/>
+			<Route
+				path={`${match.path}/:collectionId`}
+				component={CollectionContainer}
+				// ? 👆 before 👇
+				// render={(props) => (
+				// 	<CollectionsPageWithSpinner
+				// 		isLoading={!isCollectionsLoaded}
+				// 		  ? 👆 we reverse the boolean since we want to stop loading when true;					 							there's an object so we want to stop spinning
+				// 		{...props}
+				// 	/>
+				// )}
+			/>
+		</div>
+	)
+	// *** BEFORE *** 👇
+	// <Route exact path={`${match.path}`} component={CollectionsOverview} />
+	// <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+	// ? we put render={} in the <Route/> props instead of component={}; edited in folder (17.HOC, Video.2)
 }
 
 const mapDispatchToProps = (dispatch) => ({
